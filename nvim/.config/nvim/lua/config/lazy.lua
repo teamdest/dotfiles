c98a86fa529512b560e981b47ee47a3988e21f39
Patrick Barletta <teamdest@pbarletta.com>
2025-09-22 08:13:28 -0700

    vim

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.editor.neo-tree" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.formatting.biome" },
---
+++ nvim/.config/nvim/lua/plugins/replace-explorer-with-neotree.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      close_if_last_window = true,
+      sort_case_insensitive = true,
+      window = {
+        mappings = {
+          ["P"] = {
+            "toggle_preview",
+            config = {
+              use_float = true,
+              -- use_image_nvim = true,
+              -- use_snacks_image = true,
+              -- title = 'Neo-tree Preview',
+            },
+          },
+        },
+      },
+      filesystem = {
+        follow_current_file = {
+          enabled = false,
+        },
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          never_show = {
+            "DS_Store",
+            "thumbs.db",
+          },
+        },
+      },
+      buffers = {
+        follow_current_file = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
