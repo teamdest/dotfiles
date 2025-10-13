4eeb5f1f5e07a7aa305d43bd1192483438053bfe
Patrick Barletta <>
2025-10-13 09:29:32 -0700

    fish

--- fish/.config/fish/conf.d/01-path.fish
+++ fish/.config/fish/conf.d/01-path.fish
@@ -4,12 +4,12 @@ end
 if test -d /usr/local/sbin/
     fish_add_path /usr/local/sbin
 end
-if test -d {$XDG_CONFIG_HOME}/cargo/bin
+if test -d $XDG_CONFIG_HOME/cargo/bin
     fish_add_path {$XDG_CONFIG_HOME}/cargo/bin
 end
-if test -d {$HOME}/.krew/bin
+if test -d $HOME/.krew/bin
     fish_add_path {$HOME}/.krew/bin
 end
-if test -d {$HOME}/.local/bin
+if test -d $HOME/.local/bin
     fish_add_path {$HOME}/.local/bin
 end
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,19 @@
+if ! set -q XDG_CONFIG_HOME
+    set -gx XDG_CONFIG_HOME $HOME/.config
+end
+if ! set -q XDG_DATA_HOME
+    set -gx XDG_DATA_HOME $HOME/.local/share
+end
+if ! set -q XDG_CACHE_HOME
+    set -gx XDG_CACHE_HOME $HOME/.local/state
+end
 # set language
 set -gx LANG en_US.UTF-8
 set -gx LC_CTYPE en_US.UTF-8
 
 # setup base dirs for tools
-set -gx RUSTUP_HOME $XDG_CONFIG_HOME/rustup
-set -gx CARGO_HOME $XDG_CONFIG_HOME/cargo
+set -gx RUSTUP_HOME {$XDG_CONFIG_HOME}/rustup
+set -gx CARGO_HOME {$XDG_CONFIG_HOME}/cargo
 
 # configure tokens/keys
 set -gx SOPS_AGE_KEY_FILE $HOME/.config/age/age-personal
@@ -20,6 +29,8 @@ set -gx VISUAL nvim
 set -gx EDITOR nvim
 set -gx PAGER bat
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+set -gx MANROFFOPT -c
 
 fish_vi_key_bindings
 fish_config theme choose "Catppuccin Mocha"
