f73686fabc692e05035cbc49954e2676eddb7502
Patrick Barletta <>
2026-05-03 10:22:47 -0700

    chore(wezterm): update config format

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,19 +1,20 @@
 local wezterm = require("wezterm")
-local config = {}
-
-config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font("FiraCode Nerd Font")
-config.enable_tab_bar = true
-config.use_fancy_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
-config.tab_bar_at_bottom = true
-config.term = "wezterm"
-config.window_padding = {
-	left = 2,
-	right = 2,
-	top = 0,
-	bottom = 0,
+local config = {
+	color_scheme = "Catppuccin Mocha",
+	font = wezterm.font("FiraCode Nerd Font"),
+	font_size = 14.0,
+	enable_tab_bar = true,
+	use_fancy_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
+	tab_bar_at_bottom = true,
+	term = "wezterm",
+	window_padding = {
+		left = 2,
+		right = 2,
+		top = 0,
+		bottom = 0,
+	},
+	window_background_opacity = 0.95,
 }
 
-config.window_background_opacity = 0.95
 return config
